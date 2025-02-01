44a32fbd09af01582b10ba545a89ef90a2ceed74
indianup <jaibajarangclasses@gmail.com>
2025-02-02 01:20:09 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set environment variables
+export TELEGRAM_BOT_TOKEN="YOUR_TELEGRAM_BOT_TOKEN"
+export LOGIN_URL="https://example.com/login"
+export COURSE_URL="https://example.com/course"
+export WEBSITE_USERNAME="your_username"
+export WEBSITE_PASSWORD="your_password"
+
+# Run the bot
+python main.py
