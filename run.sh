89bc052a5ffcaf30158a3e9d52e80dda884c4bf9
indianup <jaibajarangclasses@gmail.com>
2025-02-02 01:31:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set environment variables
-export TELEGRAM_BOT_TOKEN="YOUR_TELEGRAM_BOT_TOKEN"
-export LOGIN_URL="https://example.com/login"
+export TELEGRAM_BOT_TOKEN="7979826252:AAG6PkktURFL-udAd3KipzwiFXp6FEQDbCg"
+export LOGIN_URL="https://app.khanglobalstudies.com/"
 export COURSE_URL="https://example.com/course"
 export WEBSITE_USERNAME="your_username"
 export WEBSITE_PASSWORD="your_password"
